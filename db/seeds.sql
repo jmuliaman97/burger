77241f7aaf1e271c8f86597c39b6128693a299d9
Jordan M <jordan.muliaman@gmail.com>
2020-05-27 15:47:35 -0700

    added folders

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+  ('Hamburger', true),
+  ('Steak burger', false),
+  ('Cheeseburger', true),
+  ('Rice burger', false),
+  ('Chicken burger', true)
